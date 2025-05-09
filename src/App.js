4db0b81055457f11864fb8f3193a41bd4146b17f
Nguyen Van Ro <57189106+nguyenvanro@users.noreply.github.com>
2025-05-09 11:17:07 +0700

    Setup redux to pj (#1)

--- src/App.js
+++ src/App.js
@@ -1,15 +1,43 @@
 import logo from './logo.svg';
 import './App.css';
+import { connect } from "react-redux"
 
-function App() {
+import {
+  increaseCounter,
+  decreaseCounter,
+} from "./action/actions"
+
+
+function App(props) {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <h1>Hello world with React and Hoi Dan IT!</h1>
+        <div>Count: {props.count}</div>
+
+      <button onClick={() => props.increaseCounter()}>Increase Count</button>
+
+      <button onClick={() => props.decreaseCounter()}>Decrease Count</button>
+      
       </header>
     </div>
   );
 }
 
-export default App;
+const mapStateToProps = state => {
+  return {
+    count: state.counter.count,
+  }
+}
+
+const mapDispatchToProps = dispatch => {
+  return {
+    increaseCounter: () => dispatch(increaseCounter()),
+
+    decreaseCounter: () => dispatch(decreaseCounter()),
+  }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(App)
+
