5db131d698ffb7de4c8ed08789e1e249d9a29dbe
Nguyen Van Ro <57189106+nguyenvanro@users.noreply.github.com>
2025-05-12 15:45:50 +0700

    Fetch data using Redux (#10)

--- src/reducer/rootReducer.js
+++ src/reducer/rootReducer.js
@@ -1,10 +1,11 @@
 import { combineReducers } from 'redux';
 import counterReducer from '../reducer/counterReducer';
-
+import UserReducer from './userReducer';
 
 const rootReducer = combineReducers({
 
     counter: counterReducer,
+    user: UserReducer,
 
 });
 
