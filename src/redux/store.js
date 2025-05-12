4e860cd70767787d9c0d98a0e8fb1fa587773635
ronguyen1998 <ronguyen1998@gmail.com>
2025-05-12 14:40:26 +0700

    Setup redux thunk

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,14 +1,14 @@
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
 import rootReducer from '../reducer/rootReducer';
 // includes: reducers, middleware...
 import { composeWithDevTools } from 'redux-devtools-extension';
-
+import thunkMiddleware from 'redux-thunk';
 
 const store = createStore(
 
     rootReducer,
 
-    composeWithDevTools(),
+    composeWithDevTools(applyMiddleware(thunkMiddleware)),
 
 );
 
