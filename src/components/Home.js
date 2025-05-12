6fe3ebcac3620fb1a7d681caf54fc2d94e5df0e6
ronguyen1998 <ronguyen1998@gmail.com>
2025-05-12 14:04:05 +0700

    Design layout react bootstrap

---
+++ src/components/Home.js
@@ -0,0 +1,19 @@
+import Header from './Header';
+import TableUser from './TableUser';
+import FormAddNew from './FormAddNew';
+
+const Home = (props) => {
+    return (
+        <>
+            <Header />
+
+            <FormAddNew />
+
+            <TableUser />
+
+        </>
+
+    )
+}
+
+export default Home;
