3d7d026cf69f28957b4b3ce9dbb353f9437d1fef
Nguyen Van Ro <57189106+nguyenvanro@users.noreply.github.com>
2025-05-13 11:38:13 +0700

    Create new user with redux (#12)

--- src/reducer/userReducer.js
+++ src/reducer/userReducer.js
@@ -1,15 +1,14 @@
 import {
-  FETCH_USER_REQUEST,
-  FETCH_USER_SUCCESS,
-  FETCH_USER_ERROR
+  FETCH_USER_REQUEST, FETCH_USER_SUCCESS, FETCH_USER_ERROR,
+  CREATE_USER_REQUEST, CREATE_USER_SUCCESS, CREATE_USER_ERROR
 } from '../action/types';
-
 
 const INITIAL_STATE = {
 
   ListUsers: [],
   isLoading: false,
-  isError: false
+  isError: false,
+  isCreating: false
 };
 
 const UserReducer = (state = INITIAL_STATE, action) => {
@@ -45,6 +44,20 @@ const UserReducer = (state = INITIAL_STATE, action) => {
 
       };
 
+    case CREATE_USER_REQUEST:
+      return {
+        ...state, isCreating: true
+      }
+
+    case CREATE_USER_SUCCESS:
+      return {
+        ...state, isCreating: false
+      }
+    
+    case CREATE_USER_ERROR:
+      return {
+        ...state, isCreating: false
+      }
 
     default: return state;
 
